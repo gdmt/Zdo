8bb3dfd78f99c9ea54caa8e564a41fd1984d2674
gdmt <dimalta@live.com>
2015-12-07 20:24:47 +0100

    Create example.php

---
+++ example.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'Zdo.php';
+
+$db=new Zdo(array(
+	'adapter'	=>'mysql',
+	'host'		=>'localhost',
+	'username'	=>'xxxxxxxx',
+	'password'	=>'xxxxxxxx',
+	'dbname'	=>'testdb',
+));
+
+$q=$db->query('SELECT name, colour, calories FROM fruit WHERE calories<?', 150);
+while($r=$q->fetchAssoc()){
+	echo $r['name'].' '.$r['colour'].' '.$r['calories']."\n";
+}
